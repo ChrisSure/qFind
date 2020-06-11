3ee0e114ef680050e58ff3d5e5eab3449a3dd273
tkuku911 <tkuku@softserveinc.com>
2020-06-11 16:15:32 +0300

    Add functional test update user.

--- api/tests/Functional/Controller/User/UpdateUserControllerTest.php
+++ api/tests/Functional/Controller/User/UpdateUserControllerTest.php
@@ -6,16 +6,16 @@ use App\Entity\User\User;
 use App\Tests\Functional\Base;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
-class CreateUserControllerTest extends Base
+class UpdateUserControllerTest extends Base
 {
     /**
      * @test
      */
-    public function createErrorValidation(): void
+    public function updateErrorValidation(): void
     {
         $this->signIn(User::$ROLE_ADMIN);
         $data = ['email' => ''];
-        $this->client->request('POST', '/users', $data);
+        $this->client->request('PUT', '/users/2', [], [], [], json_encode($data));
         $response = json_decode($this->client->getResponse()->getContent());
 
         $this->assertEquals($this->client->getResponse()->getStatusCode(), JsonResponse::HTTP_BAD_REQUEST);
@@ -25,11 +25,25 @@ class CreateUserControllerTest extends Base
     /**
      * @test
      */
+    public function updateNotFound(): void
+    {
+        $this->signIn(User::$ROLE_ADMIN);
+        $data = ['email' => 'super_admin@gmail.com', 'password' => '123', 'role' => User::$ROLE_ADMIN, 'status' => User::$STATUS_ACTIVE];
+        $this->client->request('PUT', '/users/235', $data);
+        $response = json_decode($this->client->getResponse()->getContent());
+
+        $this->assertEquals($this->client->getResponse()->getStatusCode(), JsonResponse::HTTP_NOT_FOUND);
+        $this->assertEquals($response->error, 'User doesn\'t exist.');
+    }
+
+    /**
+     * @test
+     */
     public function createAlreadyIssetEmail(): void
     {
         $this->signIn(User::$ROLE_ADMIN);
-        $data = ['email' => 'admin@gmail.com', 'password' => '123', 'role' => User::$ROLE_ADMIN, 'status' => User::$STATUS_ACTIVE];
-        $this->client->request('POST', '/users', $data);
+        $data = ['email' => 'super_admin@gmail.com', 'password' => '123', 'role' => User::$ROLE_ADMIN, 'status' => User::$STATUS_ACTIVE];
+        $this->client->request('PUT', '/users/2', $data);
         $response = json_decode($this->client->getResponse()->getContent());
 
         $this->assertEquals($this->client->getResponse()->getStatusCode(), JsonResponse::HTTP_BAD_REQUEST);
@@ -39,15 +53,15 @@ class CreateUserControllerTest extends Base
     /**
      * @test
      */
-    public function createSuccessfull(): void
+    public function updateSuccessfull(): void
     {
         $this->signIn(User::$ROLE_ADMIN);
         $data = ['email' => 'admin_test@gmail.com', 'password' => '123', 'role' => User::$ROLE_ADMIN, 'status' => User::$STATUS_ACTIVE];
-        $this->client->request('POST', '/users', $data);
+        $this->client->request('PUT', '/users/2', $data);
         $response = json_decode($this->client->getResponse()->getContent());
 
-        $this->assertEquals($this->client->getResponse()->getStatusCode(), JsonResponse::HTTP_CREATED);
-        $this->assertEquals($response->message, 'Created successfull');
+        $this->assertEquals($this->client->getResponse()->getStatusCode(), JsonResponse::HTTP_OK);
+        $this->assertEquals($response->message, 'Updated successfull');
 
         $this->revertChanges();
     }
@@ -57,7 +71,8 @@ class CreateUserControllerTest extends Base
         $doctrine = self::$container->get('doctrine');
         $manager = $doctrine->getManager();
         $user = $doctrine->getRepository(User::class)->findOneBy(['email' => 'admin_test@gmail.com']);
-        $manager->remove($user);
+        $user->setEmail('admin@gmail.com');
+        $manager->persist($user);
         $manager->flush();
     }
 }
