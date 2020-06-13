4b987e9414a2ac467ef5cd8e6d1004d0aed3f5cb
kris-sure <kukulyak.taras@gmail.com>
2020-06-13 17:14:48 +0300

    SetUp structure client.

---
+++ client/pages/signin/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "../../components/header/header";
+import Footer from "../../components/footer/footer";
+import styles from './styles.scss';
+
+
+const SignIn = () => {
+    return (
+        <main>
+            <Header/>
+            <aside>
+                <h1>SignIn</h1>
+            </aside>
+            <Footer />
+        </main>
+    )
+}
+
+export default SignIn
