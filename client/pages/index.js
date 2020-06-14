c4372e719f87bfa83fd5e178d425c0930d8744d7
kris-sure <kukulyak.taras@gmail.com>
2020-06-14 14:31:55 +0300

    Set up redux and install cors to api part.

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,9 +2,19 @@ import Head from 'next/head'
 import styles from './index.scss';
 import Header from "../components/header/header";
 import Footer from "../components/footer/footer";
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {getPosts} from "../redux/actions/postAction";
 
 
 export default function Home() {
+    /*const dispatch = useDispatch();
+    const {posts} = useSelector(state => state.post);
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, []);*/
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,6 +23,9 @@ export default function Home() {
       </Head>
 
       <main>
+          {/*{posts && posts.map(item => (
+              <h2 key={item}>{item}</h2>
+          ))}*/}
           <Header/>
           <Footer />
       </main>
