8d3bc99fd164a268c42656a13054b7013b8f9a9b
tkuku911 <tkuku@softserveinc.com>
2020-06-15 16:00:38 +0300

    Start template auth client.

--- client/pages/signup/index.js
+++ client/pages/signup/index.js
@@ -4,28 +4,20 @@ import Footer from "../../components/footer/footer";
 import styles from './styles.scss';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import Link from "next/link";
 
 
-const SignIn = () => {
+const SignUp = () => {
     return (
         <main>
             <Header/>
             <aside>
-                <h1>SignIn</h1>
+                <h1>SignUp</h1>
                 <form className={styles.root} noValidate autoComplete="off">
                     <ul>
                         <li><TextField id="outlined-basic" label="Email" variant="outlined" /></li>
                         <li><TextField id="outlined-basic" label="Password" variant="outlined" type="password" /></li>
                         <li>
-                            <Button variant="contained" color="primary">Login</Button>
-                        </li>
-                        <li>
-                            <Button variant="contained" color="secondary">
-                                <Link href="/signup">
-                                    <a> Create account</a>
-                                </Link>
-                            </Button>
+                            <Button variant="contained" color="primary">Create account</Button>
                         </li>
                     </ul>
                 </form>
@@ -35,4 +27,4 @@ const SignIn = () => {
     )
 }
 
-export default SignIn
+export default SignUp
--- client/redux/actions/postAction.js
+++ client/redux/actions/postAction.js
@@ -1,6 +1,6 @@
 import * as types from "../types";
 
-export const getposts = () =>async dispatch=>{
+export const getPosts = () =>async dispatch=>{
     dispatch({
         type: types.GET_POSTS,
         payload: ['1post', '2post'],
