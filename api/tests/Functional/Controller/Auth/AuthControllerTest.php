819602edc23a2893a0c824970475a336035a9e61
kris-sure <kukulyak.taras@gmail.com>
2020-07-05 23:13:29 +0300

    Create signup user functional test.

--- api/tests/Functional/Controller/Auth/AuthControllerTest.php
+++ api/tests/Functional/Controller/Auth/AuthControllerTest.php
@@ -2,7 +2,9 @@
 
 namespace App\Tests\Functional\Controller\Auth;
 
+use App\Entity\User\User;
 use App\Tests\Functional\Base;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AuthControllerTest extends Base
 {
@@ -47,19 +49,46 @@ class AuthControllerTest extends Base
     /**
      * @test
      */
-    /*public function signUpAdmin(): void
+    public function signUpUser(): void
     {
-        $postData = ['email' => 'admin@gmail.com', 'password' => '123', 'type' => 'admin'];
+        $postData = ['email' => $email = 'user2@gmail.com', 'password' => '123', 'type' => 'site'];
 
         $this->client->request(
             'POST',
-            '/auth/signin',
+            '/auth/signup',
             $postData
         );
 
         $response = json_decode($this->client->getResponse()->getContent());
 
         $this->assertTrue($this->client->getResponse()->isSuccessful());
-        $this->assertTrue(is_string($response->token));
-    }*/
+        $this->assertEquals($response->message, 'For confirm registration check your email');
+        $this->revertChanges($email);
+    }
+
+    /**
+     * @test
+     */
+    public function signUpIssetUser(): void
+    {
+        $postData = ['email' => 'user@gmail.com', 'password' => '123', 'type' => 'site'];
+
+        $this->client->request(
+            'POST',
+            '/auth/signup',
+            $postData
+        );
+
+        $response = json_decode($this->client->getResponse()->getContent());
+
+        $this->assertEquals($this->client->getResponse()->getStatusCode(), JsonResponse::HTTP_NOT_FOUND);
+        $this->assertEquals($response->error, 'User who has this email already exists.');
+    }
+
+    private function revertChanges($email)
+    {
+        $user = $this->doctrine->getRepository(User::class)->findOneBy(['email' => $email]);
+        $this->manager->remove($user);
+        $this->manager->flush();
+    }
 }
